bacc7e9b2009c949260d2a944b6f830797b2ad61
matheusbtguerra <matheus05guerra@gmail.com>
2025-01-21 00:41:20 -0300

    Project created

---
+++ src/main/java/com/devSuperior/DSlist/DSlistApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.DSlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DSlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DSlistApplication.class, args);
+	}
+
+}
